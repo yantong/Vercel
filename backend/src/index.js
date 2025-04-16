143347269a1a839a0a2c0e2a48678d87a144771c
leaf <1209144177@qq.com>
2025-04-16 20:25:07 +0800

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,20 @@
+const path = require('path');
+
 const express = require("express");
 const app = express();
 
 app.use(express.static('public'));
 
 app.get("/api/qiuye", (req, res) => res.send("秋叶啊秋叶"));
+
+
+app.get('/', (req, res) => {
+    const currentFilePath = __filename;
+    const currentDirPath = path.dirname(currentFilePath);
+    const parentDirPath = path.resolve(currentDirPath, '..');
+
+    res.sendFile(parentDirPath + '/public/index.html');
+})
 
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
