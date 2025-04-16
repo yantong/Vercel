13d147526957fa70a5404cd53191b9a421e0e0eb
leaf <1209144177@qq.com>
2025-04-16 20:27:47 +0800

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,17 @@ const path = require('path');
 const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+const currentFilePath = __filename;
+const currentDirPath = path.dirname(currentFilePath);
+const parentDirPath = path.resolve(currentDirPath, '..');
+
+app.use(express.static(parentDirPath + '/public'));
 
 app.get("/api/qiuye", (req, res) => res.send("秋叶啊秋叶"));
 
 
 app.get('/', (req, res) => {
-    const currentFilePath = __filename;
-    const currentDirPath = path.dirname(currentFilePath);
-    const parentDirPath = path.resolve(currentDirPath, '..');
+
 
     res.sendFile(parentDirPath + '/public/index.html');
 })
