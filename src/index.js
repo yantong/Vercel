3257d1c0cfb5b5abc2db1f728bfb38413f4b5990
yantong <yantong@winning.com.cn>
2026-02-11 13:16:16 +0800

    又又调整

--- src/index.js
+++ src/index.js
@@ -27,9 +27,7 @@ const client = new OpenAI({
 });
 
 const initialModelReqCountMap = {
-    "deepseek-ai/DeepSeek-V3.2": 100,
-    "deepseek-ai/DeepSeek-R1-0528": 100,
-    "meituan-longcat/LongCat-Flash-Lite": "500",
+    "Qwen/Qwen3-Next-80B-A3B-Instruct": "500",
     "moonshotai/Kimi-K2.5": 50,
 };
 
