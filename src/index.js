1269cd9085db21e152e0bab1097eaee1daf5abf9
leaf <1209144177@qq.com>
2026-02-12 13:51:22 +0800

    改回去算了

--- src/index.js
+++ src/index.js
@@ -388,8 +388,6 @@ app.post("/analysis", async (req, res) => {
   await callAnalysis(res, lyrics);
 });
 
-app.get("/hello", (req, res) => res.send("hello word"));
-
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 exports.app = app;
